69d3d5440bc8accd21af97b36ddb0c1b8a26b1f8
Nikolai <kkt1337@gmail.com>
2023-01-31 20:53:07 +0300

    Добавлена возможность скачивать фото из БД по ссылке.

--- common-jpa/src/main/java/homeproject/dao/AppDocDAO.java
+++ common-jpa/src/main/java/homeproject/dao/AppDocDAO.java
@@ -2,6 +2,9 @@ package homeproject.dao;
 
 import homeproject.entity.AppDoc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public interface AppDocDAO extends JpaRepository<AppDoc, Long> {
 }
--- common-jpa/src/main/java/homeproject/dao/AppPhotoDAO.java
+++ common-jpa/src/main/java/homeproject/dao/AppPhotoDAO.java
@@ -2,6 +2,9 @@ package homeproject.dao;
 
 import homeproject.entity.AppPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public interface AppPhotoDAO extends JpaRepository<AppPhoto,Long> {
 }
